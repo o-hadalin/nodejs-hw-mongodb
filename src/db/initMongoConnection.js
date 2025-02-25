beee35847ff309efae9dbb99aaab62b87560b806
O. HADALIN <dyblikat777@gmail.com>
2025-02-25 16:03:08 +0200

    add schema, dell old param from initMongoConnect

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -7,10 +7,7 @@ const initMongoConnection = async () => {
   try {
     const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
 
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(uri);
 
     console.log('Mongo connection successfully established!');
   } catch (error) {
