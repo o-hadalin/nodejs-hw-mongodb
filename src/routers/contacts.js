b8ae017e09988176551d58f65702ad7d95e934da
O. HADALIN <dyblikat777@gmail.com>
2025-03-21 22:39:18 +0200

    add authentication middleware and protect contacts routes

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -3,27 +3,31 @@ import contactController from '../controllers/contacts.js';
 import ctrlWrapper from '../utils/ctrlWrapper.js';
 import validateBody from '../middlewares/validateBody.js';
 import isValidId from '../middlewares/isValidId.js';
-import { contactSchema, updateContactSchema } from '../validation/contacts.js';
+import * as validation from '../validation/contact.js';
 
 const router = Router();
 
 router.get('/', ctrlWrapper(contactController.getContacts));
+
 router.get(
   '/:contactId',
   isValidId,
   ctrlWrapper(contactController.getContactById),
 );
+
 router.post(
   '/',
-  validateBody(contactSchema),
+  validateBody(validation.contactSchema),
   ctrlWrapper(contactController.createContact),
 );
+
 router.patch(
   '/:contactId',
   isValidId,
-  validateBody(updateContactSchema),
+  validateBody(validation.updateContactSchema),
   ctrlWrapper(contactController.patchContact),
 );
+
 router.delete(
   '/:contactId',
   isValidId,
