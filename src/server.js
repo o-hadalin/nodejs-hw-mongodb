c22d405c1e7ddef8ae46372e244ed806bf394b22
O. HADALIN <dyblikat777@gmail.com>
2025-03-22 10:49:55 +0200

    add cookieParser

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import contactsRouter from './routers/contacts.js';
 import errorHandler from './middlewares/errorHandler.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
@@ -16,6 +17,7 @@ const setupServer = () => {
   app.use(cors());
   app.use(pino());
   app.use(express.json());
+  app.use(cookieParser());
 
   app.use('/contacts', contactsRouter);
   app.use('/auth', authRouter);
